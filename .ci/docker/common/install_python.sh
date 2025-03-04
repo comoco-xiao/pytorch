a094ad603dde5e4db7c725bda4f4324deb01dc00
Catherine Lee <csl@fb.com>
2025-03-04 11:04:43 -0800

    tc

--- .ci/docker/common/install_python.sh
+++ .ci/docker/common/install_python.sh
@@ -2,16 +2,13 @@
 set -ex
 
 apt-get update
+sudo add-apt-repository ppa:deadsnakes/ppa
 apt-get install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python3-pip python${PYTHON_VERSION}-venv
 
 ln -s /usr/bin/python${PYTHON_VERSION} /usr/bin/python
-# alias python=python${PYTHON_VERSION}
 python -m venv /var/lib/jenkins/lint
-chown jenkins:jenkins /var/lib/jenkins/lint
-
 source /var/lib/jenkins/lint/bin/activate
-python --version
-python3 --version
+
 python -mpip install --upgrade pip
 python -mpip install -r /opt/requirements-ci.txt
-python -mpip install cmake
+python -mpip install cmake==3.31.6
