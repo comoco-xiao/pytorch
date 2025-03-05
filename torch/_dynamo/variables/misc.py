0cebfcd6e071f4fe06d3ae490d9ec251fa26f4bd
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-03-04 21:56:03 -0300

    Add `__context/cause/suppress_context/traceback__` to Exception
    ghstack-source-id: 17cfc7c594bab75b1224e2a1a14520136b92fa4a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146499

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -50,6 +50,7 @@ from ..utils import (
     tuple_methods,
 )
 from .base import VariableTracker
+from .constant import ConstantVariable
 from .functions import NestedUserFunctionVariable, UserFunctionVariable
 from .user_defined import call_random_fn, is_standard_setattr, UserDefinedObjectVariable
 
@@ -274,19 +275,106 @@ class ExceptionVariable(VariableTracker):
         super().__init__(**kwargs)
         self.exc_type = exc_type
         self.args = args
+        # When raising a new exception while another exception is already being
+        # handled, the new exception's __context__ attribute is automatically
+        # set to the handled exception.
+        self.__context__ = ConstantVariable(None)
+        # Set when user raised an exception from another:
+        # raise ... from ...
+        self.__cause__ = ConstantVariable(None)
+        # Boolean flag that controls whether the __context__ attribute is set
+        self.__suppress_context__ = ConstantVariable(False)
+        # Contains the call stack where the exception was raised. Dynamo does
+        # not track traceback. So, this variable is always set to None
+        self.__traceback__ = ConstantVariable(None)
+
+    def set_context(self, context: "ExceptionVariable"):
+        self.__context__ = context
 
     def reconstruct(self, codegen):
+        if type(self.__context__) is not ConstantVariable:
+            unimplemented("ExceptionVariable with __context__")
         codegen.add_push_null(
             lambda: codegen.load_import_from("builtins", self.exc_type.__name__)
         )
         codegen.foreach(self.args)
         codegen.call_function(len(self.args), False)
 
-    def __str__(self):
-        return f"ExceptionVariable({self.exc_type})"
+    def python_type(self):
+        return self.exc_type
 
-    def __repr__(self):
-        return f"ExceptionVariable({self.exc_type})"
+    def call_setattr(
+        self,
+        tx: "InstructionTranslator",
+        name_var: VariableTracker,
+        val: VariableTracker,
+    ):
+        def raise_error(msg):
+            raise_observed_exception(TypeError, tx, args=[ConstantVariable(msg)])
+
+        name = name_var.as_python_constant()
+        if name == "__context__":
+            self.set_context(val)
+        elif name == "__cause__":
+            if (isinstance(val, ConstantVariable) and val.value is None) or isinstance(
+                val,
+                (
+                    variables.BuiltinVariable,
+                    variables.ExceptionVariable,
+                    variables.UserDefinedExceptionClassVariable,
+                    variables.UserDefinedExceptionObjectVariable,
+                ),
+            ):
+                self.__cause__ = val
+                self.__suppress_context__ = variables.ConstantVariable(True)
+            else:
+                raise_error("exception cause must be None or derive from BaseException")
+        elif name == "__suppress_context__":
+            if isinstance(val, ConstantVariable) and val.value in (True, False):
+                self.__suppress_context__ = val
+            else:
+                raise_error("exception cause must be None or derive from BaseException")
+        elif name == "__traceback__":
+            if isinstance(val, ConstantVariable) and val.value is None:
+                self.__traceback__ = val
+            else:
+                unimplemented(f"setattr(ExceptionVariable, {name_var}, {val})")
+        else:
+            unimplemented(f"setattr(ExceptionVariable, {name_var}, {val})")
+        return variables.ConstantVariable(None)
+
+    def call_method(self, tx, name, args, kwargs):
+        if name == "__setattr__":
+            return self.call_setattr(tx, *args)
+        elif name == "with_traceback":
+            [tb] = args
+            self.call_setattr(tx, ConstantVariable("__traceback__"), tb)
+            return self
+        else:
+            return super().call_method(tx, name, args, kwargs)
+
+    def var_getattr(self, tx, name):
+        if name == "__context__":
+            return self.__context__
+        elif name == "__cause__":
+            return self.__cause__
+        elif name == "__suppress_context__":
+            return self.__suppress_context__
+        elif name == "__traceback__":
+            return variables.ConstantVariable(None)
+        elif name == "args":
+            return variables.ListVariable(self.args, source=self.source)
+        return super().var_getattr(tx, name)
+
+    def __str__(self):
+        ctx = self.__context__
+        if type(ctx) is variables.ConstantVariable:
+            ctx = None
+        else:
+            ctx = ctx.exc_type.__name__
+        return f"{self.__class__.__name__}({self.exc_type.__name__}, context={ctx})"
+
+    __repr__ = __str__
 
 
 class UnknownVariable(VariableTracker):
